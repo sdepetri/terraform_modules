0cb328110c258634d00f845b17473095924bcfe8
sdepetri <sdepetri@gmail.com>
2024-10-23 04:57:55 -0300

    en el modulo se-ecs comente el depends on al loab banlancer, porque nose porque no lo puedo enlazar. en fin, ahi pasa el plan, pero luego no se enlaza en el despliegue

--- modules/sd-ecs/main.tf
+++ modules/sd-ecs/main.tf
@@ -50,9 +50,9 @@ resource "aws_ecs_service" "sd_service" {
     container_name   = "sd-container"
     container_port   = 80
   }
-    depends_on = [
-    module.alb
-  ]
+  #   depends_on = [
+  #   module.alb
+  # ]
 }
 
 # resource "aws_cloudwatch_metric_alarm" "service_cpu" {
