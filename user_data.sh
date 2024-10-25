74db526a0331b7e2fab644f599a048ea6ec25d48
sdepetri <sdepetri@gmail.com>
2024-10-24 22:09:41 -0300

    user_data.sh harcodeado

--- user_data.sh
+++ user_data.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 echo ECS_CLUSTER=${var.cluster_name} >> /etc/ecs/ecs.config
+#asi es generico, en el problema la variable iba vacia y se asociaba al un cluster "default" y eso era el problema mas grande.
